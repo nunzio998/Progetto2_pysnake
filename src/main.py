86732f38b604fd96b94303f89755d440cfa50156
Gabriele Giordano <giordano.gabriele@outlook.it>
2022-12-30 11:38:12 +0100

    implemented basic structure in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,29 @@
+import json
+
+
 def play(game_file: str) -> int:
-  # scrivi qui il tuo codice
-  pass
+    """
+
+    :param game_file: str, path relativo del file in formato .json contenente le informazioni sulla partita
+    :return: int, lunghezza finale del corpo del serpente
+    """
+
+# SETUP
+    game = open(game_file)
+    game_dict = json.load(game)
+
+    #import dynamic field from .png or .json
+
+    #import moves
+
+# GAME
+    #check next move
+
+    #if next move is legal, update dynamic field
+
+    #else end game
+
+# OUT
+    return -1
+
+play("data/gamefile_01.json")
