ce23b9d5c654b4e5bd90c4d6f66c8e159bb80a76
francesco <francesco.pinsone@alcampus.it>
2023-01-02 22:58:34 +0100

    constructor added to Snake class

---
+++ src/snake.py
@@ -0,0 +1,8 @@
+class Snake:
+    def __init__(self, x_iniziale, y_iniziale):
+        self.lunghezza = 1
+        self.x_testa = x_iniziale
+        self.y_testa = y_iniziale
+
+    def muovi(self, mossa: str):
+        pass
