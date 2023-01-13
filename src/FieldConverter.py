a1977aa1fa9b8936e783966f9bc871756a36f651
Matteo Gardusi <matteo.gardusi@gmail.com>
2023-01-13 17:58:04 +0100

    ADD FieldMapper class structure

---
+++ src/FieldConverter.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class FieldColor(Enum):
+    EMPTY = 0
+    SNAKE = 1
+    SNAKE_TRAIL = 2
+    FOOD = 3
+    BLOCK = 4
+
+
+class FieldMapper:
+    def __init__(self):
+
+    def RGB_to_number(self):
+        pass
+
+    def number_to_RGB(self):
+        pass
