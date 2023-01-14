a98ad0d8e0dcc5be4e91750b91669c3e58c58be8
francesco <francesco.pinsone@alcampus.it>
2023-01-14 14:30:35 +0100

    main running test for Snake

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
+from src.Snake import Snake
+
+
 def play(game_file: str) -> int:
-  # scrivi qui il tuo codice
-  pass
+    # scrivi qui il tuo codice
+    pass
+
+
+if __name__ == "__main__":
+    snake = Snake((1, 2))
+    snake.move((1, 3))
+    snake.move((2, 4))
+    snake.eat((2, 5))
+    snake.move((2, 6))
+    snake.eat((2, 7))
+    snake.move((3, 6))
+    print(snake.get_body())
+    print(snake.get_trail())
+    print(snake.get_head())
