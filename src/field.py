4aa2bfb8ab132595910193190b3b5ba43edf7bdd
Gabriele Giordano <giordano.gabriele@outlook.it>
2023-01-19 14:20:39 +0100

    comments update

--- src/field.py
+++ src/field.py
@@ -3,14 +3,14 @@ import numpy as np
 
 class Field:
     """
-    Classe che definisce il campo da gioco, rappresentato con un numpy.ndarray bidimensionale in cui il valore di ogni
+    Classe che definisce il campo da gioco, rappresentato con un numpy.ndarray bidimensionale; il valore di ogni
     elemento indica il contenuto di una casella del campo, in accordo con la codifica stabilita nella classe FieldColor.
 
     Attributi:
         field: numpy.ndarray, array bidimensionale rappresentante il contenuto delle caselle del campo da gioco;
-        le caselle sono identificate nel campo da una coppia (riga,colonna):
-        - le righe sono crescenti verso il basso
-        - le colonne sono crescenti verso destra
+        ogni casella è identificata dalla sua posizine, rappresentata con una coppia di indici (riga,colonna), in cui:
+        - le righe partono da zero e sono crescenti verso il basso
+        - le colonne partono da zero e sono crescenti verso destra
         size:  tuple of ints, dimensioni del campo da gioco: (numero righe, numero colonne)
 
     Metodi:
