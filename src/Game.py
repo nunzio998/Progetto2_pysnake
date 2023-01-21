8d094bfafc0e985943b9d7201823bb1b8f1eec77
Matteo Gardusi <matteo.gardusi@gmail.com>
2023-01-21 11:16:23 +0100

    ADD implement method that manages the presence of obstacles or food on the initial position of the snake

--- src/Game.py
+++ src/Game.py
@@ -24,7 +24,7 @@ class Game:
         self.moves = moves
 
     @classmethod
-    def check_start(cls, *args) -> bool:  # rivedere parametri di ingresso
+    def check_start(cls, field, snake) -> bool:
         """
         Metodo che esegue un controllo sulle condizioni iniziali della partita.
             * se start è su cibo: snake mangia subito
@@ -32,9 +32,22 @@ class Game:
             * se start è fuori dal campo: snake muore subito con lunghezza 1
 
         Se il serpente muore subito restituisce True
+
+        :param field: Campo da gioco sul quale si sta giocando
+        :param snake: Serpente con cui si sta giocando
         :return:
         """
-        pass
+        size = field.get_size()  # dimensioni del campo
+        start = snake.get_head()  # posizione iniziale del serpente
+        if 0 <= start[0] < size[0] and 0 <= start[1] < size[1]:  # se start è all'interno del campo da gioco
+            what_in_start = field.get_value(start)
+            match what_in_start:
+                case FieldColor.FOOD.value:
+                    snake.eat(start)
+                case FieldColor.BLOCK.value:
+                    return True
+            return False  # se in start c'è campo vuoto o cibo, si continua a giocare
+        return True  # se start è al di fuori del campo da gioco
 
     def run(self) -> int:
         """
@@ -43,8 +56,8 @@ class Game:
 
         :return: int, lunghezza finale del serpente
         """
-        if Game.check_start():  # aggiungere parametri di ingresso
-            return -1  # rivedere il return, deve restituire la lunghezza finale del serpente
+        if Game.check_start(self.field, self.snake):
+            return self.snake.lenght  # rivedere il return, deve restituire la lunghezza finale del serpente
 
         # per ogni mossa
         for move in self.moves:
